33da0129df1516f084afd8e93c3ea4ee1aad2e74
Bernd Mueller <mueller@b1-systems.de>
2021-06-24 08:40:42 +0200

    remove oracle linux code
    Signed-off-by: Bernd Mueller <mueller@b1-systems.de>

--- scripts/centos-stream/update.sh
+++ scripts/centos-stream/update.sh
@@ -10,12 +10,5 @@ else
   yum -y update
 fi
 
-# Updating the oracle release on at least OL 6 updates the repos and unlocks a whole
-# new set of updates that need to be applied. If this script is there it should be run
-if [ -f "/usr/bin/ol_yum_configure.sh" ]; then
-  /usr/bin/ol_yum_configure.sh
-  yum -y update
-fi
-
 reboot;
 sleep 60;
