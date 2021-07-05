d465b88050d72131ba5b5ed4fb5e54ee9e5f780e
Bernd Mueller <mueller@b1-systems.de>
2021-07-05 14:31:01 +0200

    call publish script
    Signed-off-by: Bernd Mueller <mueller@b1-systems.de>

--- cirrus-publish.sh
+++ cirrus-publish.sh
@@ -5,10 +5,10 @@
 if [ $CIRRUS_BRANCH == 'main' ]; then
   cd ./images/
   ls ./
-  echo $CIRRUS_BRANCH "-"  ${S3CMD_HOST}
-  #s3cmd --host="${S3CMD_HOST}" --host-bucket="${S3CMD_HOST}" --access_key="${S3CMD_ACCESS_KEY}" --secret_key="${S3CMD_SECRET_KEY}" sync . "s3://${S3CMD_BUCKET}"
+  echo "main -" $CIRRUS_BRANCH
+  s3cmd --host="${S3CMD_HOST}" --host-bucket="${S3CMD_HOST}" --access_key="${S3CMD_ACCESS_KEY}" --secret_key="${S3CMD_SECRET_KEY}" sync . "s3://${S3CMD_BUCKET}"
 else
   cd ./images/
   ls ./
-  echo "nicht main -" $CIRRUS_BRANCH "-"  ${S3CMD_HOST}
+  echo "not main -" $CIRRUS_BRANCH
 fi
