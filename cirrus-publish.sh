93eb7ed28a96100d5da62e27e2960b28a7bf1a21
Bernd Mueller <mueller@b1-systems.de>
2021-07-05 14:22:23 +0200

    call publish script
    Signed-off-by: Bernd Mueller <mueller@b1-systems.de>

--- cirrus-publish.sh
+++ cirrus-publish.sh
@@ -2,5 +2,9 @@
 
 # script to publish/upload images to images.osism.io
 
-ls ./images/
-echo $CIRRUS_BRANCH
+if [ $CIRRUS_BRANCH == 'main' ]; then
+  cd ./images/
+  ls ./
+  echo $CIRRUS_BRANCH "-"  ${S3CMD_HOST}
+  #s3cmd --host="${S3CMD_HOST}" --host-bucket="${S3CMD_HOST}" --access_key="${S3CMD_ACCESS_KEY}" --secret_key="${S3CMD_SECRET_KEY}" sync . "s3://${S3CMD_BUCKET}"
+fi
