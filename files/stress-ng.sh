8d4e02d1fcf4495754da304a16b4e318a5fff07a
Christian Berendt <berendt@osism.tech>
2025-02-24 20:22:52 +0100

    Also test memory with stress-ng
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/stress-ng.sh
+++ files/stress-ng.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 NUMBER_OF_CPUS=$(nproc --all)
-/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 86400s
+/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 3600s
+/usr/bin/stress-ng --vm 1 --vm-bytes 50% --vm-method all --verify --timeout 600s
