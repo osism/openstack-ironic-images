5063087ee7f43ea582786cb82eb7e2a00345771c
Christian Berendt <berendt@osism.tech>
2025-02-25 22:04:38 +0100

    Only use stress-ng for memtests (but keep memtester)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/burnin.sh
+++ files/burnin.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 NUMBER_OF_CPUS=$(nproc --all)
-MEMTOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 - 2097152 }')
 
 mkdir -p /opt/burnin
 
@@ -18,5 +17,4 @@ next=$((max + 1))
 
 /usr/sbin/dmidecode -s system-serial-number | tee -a /opt/burnin/$next
 /usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 600s | tee -a /opt/burnin/$next
-/usr/bin/stress-ng --vm 1 --vm-bytes ${MEMTOTAL}k --vm-method all --verify --timeout 600s | tee -a /opt/burnin/$next
-/usr/sbin/memtester ${MEMTOTAL}k 1 | tee -a /opt/burnin/$next
+/usr/bin/stress-ng --vm 1 --vm-bytes 80% --vm-method all --verify --timeout 600s | tee -a /opt/burnin/$next
