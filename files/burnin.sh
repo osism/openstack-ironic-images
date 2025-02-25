0b350ccdb01c6d7af0c3da2ce4bbe9f901c2f60c
Christian Berendt <berendt@osism.tech>
2025-02-25 20:18:18 +0100

    Run info script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/burnin.sh
+++ files/burnin.sh
@@ -3,6 +3,20 @@
 NUMBER_OF_CPUS=$(nproc --all)
 MEMTOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 - 32768 }')
 
-/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 600s
-/usr/bin/stress-ng --vm 1 --vm-bytes ${MEMTOTAL}k --vm-method all --verify --timeout 600s
-/usr/sbin/memtester ${MEMTOTAL}k 1
+mkdir -p /opt/burnin
+
+max=0
+for file in /opt/burnin/*; do
+    name=$(basename "$file")
+    if [[ $name =~ ^[0-9]+$ ]]; then
+        if (( name > max )); then
+            max=$name
+        fi
+    fi
+done
+next=$((max + 1))
+
+/usr/sbin/dmidecode -s system-serial-number | tee -a /opt/burnin/$next
+/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 600s | tee -a /opt/burnin/$next
+/usr/bin/stress-ng --vm 1 --vm-bytes ${MEMTOTAL}k --vm-method all --verify --timeout 600s | tee -a /opt/burnin/$next
+/usr/sbin/memtester ${MEMTOTAL}k 1 | tee -a /opt/burnin/$next
