348810648f48371024e48d16ee3cdc53d1ab87a1
Christian Berendt <berendt@osism.tech>
2025-02-25 20:25:32 +0100

    Run info script (#19)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/info.sh
+++ files/info.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-lsblk
-lspci -v
-ip l
-dmidecode
+mkdir -p /opt/info
+
+max=0
+for file in /opt/info/*; do
+    name=$(basename "$file")
+    if [[ $name =~ ^[0-9]+$ ]]; then
+        if (( name > max )); then
+            max=$name
+        fi
+    fi
+done
+next=$((max + 1))
+
+/usr/sbin/dmidecode -s system-serial-number | tee -a /opt/info/$next
+/usr/sbin/dmidecode | tee -a /opt/info/$next
+/usr/bin/ip l | tee -a /opt/info/$next
+/usr/bin/lsblk | tee -a /opt/info/$next
+/usr/bin/lspci -v | tee -a /opt/info/$next
