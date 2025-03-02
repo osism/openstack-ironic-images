9fc937e106547e51d59fe4c058711c23b7d80078
Christian Berendt <berendt@osism.tech>
2025-03-02 20:31:11 +0100

    Run for one day (#26)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/burnin.sh
+++ files/burnin.sh
@@ -5,5 +5,5 @@ NUMBER_OF_CPUS=$(nproc --all)
 filename=/opt/burnin-$(/usr/sbin/dmidecode -s system-serial-number).txt
 
 /usr/sbin/dmidecode -s system-serial-number | tee -a ${filename// /_}
-/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 600s | tee -a ${filename// /_}
-/usr/bin/stress-ng --vm 1 --vm-bytes 80% --vm-method all --verify --timeout 600s | tee -a ${filename// /_}
+/usr/bin/stress-ng --cpu $NUMBER_OF_CPUS --timeout 43200s | tee -a ${filename// /_}
+/usr/bin/stress-ng --vm 1 --vm-bytes 80% --vm-method all --verify --timeout 43200s | tee -a ${filename// /_}
