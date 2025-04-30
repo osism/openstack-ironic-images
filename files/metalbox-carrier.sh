e1d1fc5a71ff770c280199f1ce61939100ce4693
Christian Berendt <berendt@osism.tech>
2025-04-30 12:05:03 +0200

    metalbox-carrier: use /usr/bin/bash as login shell
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox-carrier.sh
+++ files/metalbox-carrier.sh
@@ -1,6 +1,7 @@
 export DIB_DEV_USER_USERNAME=dragon
 export DIB_DEV_USER_PWDLESS_SUDO=True
 export DIB_DEV_USER_PASSWORD=password
+export DIB_DEV_USER_SHELL=/usr/bin/bash
 
 export IMAGE_SIZE=8
 export IMAGE_ELEMENTS="cloud-init-nocloud devuser"
