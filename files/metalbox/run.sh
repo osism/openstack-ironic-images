ccafb80e762355b00f7db81f1f6b56fc60053bd0
Christian Berendt <berendt@osism.tech>
2025-05-02 21:35:48 +0200

    metalbox: import registry volume archive
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox/run.sh
+++ files/metalbox/run.sh
@@ -3,4 +3,11 @@
 # run docker registry
 
 docker load -i /opt/registry.tar
+
+if [[ -e /opt/registry.tar.bz2 ]]; then
+    docker load -i /opt/alpine.tar
+    docker volume create registry
+    docker run --rm -v registry:/volume -v /opt:/import sh -c 'cd /volume && tar xjf /import/registry.tar.bz2'
+fi
+
 docker run -d -p 5000:5000 -v registry:/var/lib/registry --name registry registry:3
