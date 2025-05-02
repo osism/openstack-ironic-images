7221327965ed579b6f96016e929820a3964f0005
Christian Berendt <berendt@osism.tech>
2025-05-02 19:49:43 +0200

    metalbox: add devuser and set password of operator user
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox.sh
+++ files/metalbox.sh
@@ -1,2 +1,6 @@
+export DIB_DEV_USER_PWDLESS_SUDO=True
+export DIB_DEV_USER_PASSWORD=password
+export DIB_DEV_USER_SHELL=/usr/bin/bash
+
 export IMAGE_SIZE=20
 export IMAGE_ELEMENTS="cloud-init-nocloud"
