5328aea3c803457c9f78a23eb89be1790af74cb6
Christian Berendt <berendt@osism.tech>
2025-05-04 19:41:55 +0200

    metalbox: reduce initial image size to 10 GByte
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox.sh
+++ files/metalbox.sh
@@ -2,5 +2,5 @@ export DIB_DEV_USER_PWDLESS_SUDO=True
 export DIB_DEV_USER_PASSWORD=password
 export DIB_DEV_USER_SHELL=/usr/bin/bash
 
-export IMAGE_SIZE=20
+export IMAGE_SIZE=10
 export IMAGE_ELEMENTS="cloud-init-nocloud"
