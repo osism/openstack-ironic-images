a1e70fc1ecdf662a86923b6cbba5b7800bb33d1e
Christian Berendt <berendt@osism.tech>
2025-05-08 21:20:44 +0200

    REmove metalbox carrier
    We use grml for this.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox-carrier.sh
+++
@@ -1,7 +0,0 @@
-export DIB_DEV_USER_USERNAME=dragon
-export DIB_DEV_USER_PWDLESS_SUDO=True
-export DIB_DEV_USER_PASSWORD=password
-export DIB_DEV_USER_SHELL=/usr/bin/bash
-
-export IMAGE_SIZE=3
-export IMAGE_ELEMENTS="cloud-init-nocloud devuser"
