2213882632d0336a724a8d601b961b8caaff6545
Christian Berendt <berendt@osism.tech>
2025-05-26 19:11:17 +0200

    metalbox: do not use registry.osism.tech for alpine/registry (#81)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/metalbox/run.sh
+++ files/metalbox/run.sh
@@ -5,7 +5,7 @@ docker load -i /opt/alpine.tar
 
 if [[ -e /opt/registry.tar.bz2 ]]; then
     docker volume create registry
-    docker run --rm -v registry:/volume -v /opt:/import registry.osism.tech/dockerhub/library/alpine:3 sh -c 'cd /volume && tar xjf /import/registry.tar.bz2'
+    docker run --rm -v registry:/volume -v /opt:/import library/alpine:3 sh -c 'cd /volume && tar xjf /import/registry.tar.bz2'
 fi
 
-docker run -d -p 127.0.0.1:5000:5000 -v registry:/var/lib/registry --name registry registry.osism.tech/dockerhub/library/registry:3
+docker run -d -p 127.0.0.1:5000:5000 -v registry:/var/lib/registry --name library/registry:3
