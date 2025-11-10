88ab8fe1443faf5844e42a24124c2732724a5a6d
Jan Horstmann <horstmann@osism.tech>
2025-11-10 15:18:53 +0100

    metalbox: Let registry listen on all addresses
    In environments were the metalbox is used as container registry, it
    needs to be externally accessible.
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- elements/metalbox/static/usr/local/bin/run.sh
+++ elements/metalbox/static/usr/local/bin/run.sh
@@ -8,4 +8,4 @@ if [[ -e /opt/registry.tar.bz2 ]]; then
     docker run --rm -v registry:/volume -v /opt:/import library/alpine:3 sh -c 'cd /volume && tar xjf /import/registry.tar.bz2'
 fi
 
-docker run -d -p 127.0.0.1:5001:5000 -v registry:/var/lib/registry --name registry --restart always library/registry:3
+docker run -d -p 0.0.0.0:5001:5000 -v registry:/var/lib/registry --name registry --restart always library/registry:3
